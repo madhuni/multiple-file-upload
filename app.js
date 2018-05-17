b9f269c9ae6c8cd6b91bfd11ad2fc53811247fe5
Kanishka Mohan Madhuni <kanishkamohanmadhuni@gmail.com>
2018-05-17 16:58:21 +0530

    Update form functionality

--- app.js
+++ app.js
@@ -1,30 +1,39 @@
 const formData = new FormData();
+let data = {};
 
 function uploadForm(e) {
   e.preventDefault();
 
-  console.log($);
+  console.log(data);
+
   $.ajax({
-    url: '/api/',
+    url: 'http://192.168.1.106:8000/api/v1/posts/',
     type: 'POST',
     data: formData,
-    dataType: 'json',
-    success: function(res) {
-      console.log(res.data);
+    dataType: 'formData',
+    processData: false,
+    contentType: false,
+    success: function(data) {
+      // console.log('Request successful', data);
       onClearFormData();
-      document.querySelector('.blog-form').reset();
     },
-    error: function(err) {
-      console.log(err);
+    error: function(xhr) {
+      console.log('I am in error function', xhr);
+      onClearFormData();
     }
   });
 }
 
 function onClearFormData() {
+  // console.log('onClearFormData is called bitch');
   formData.delete('title');
   formData.delete('thumbnail');
   formData.delete('images');
   formData.delete('content');
+  data = {};
+  document.querySelector("#blog-form").reset();
+
+  // console.log('current state is,', formData, data);
 }
 
 function onInputChange(e) {
@@ -34,30 +43,34 @@ function onInputChange(e) {
     case 'text':
       // console.log(e.target.value);
       formData.append('title', e.target.value);
+      data['title'] = e.target.value;
       break;
 
     case 'file':
       if (e.target.id === 'thumbnail') {
         // console.log(e.target.files[0]);
         formData.append('thumbnail', e.target.files[0]);
+        data['thumbnail'] = e.target.files[0];
       } else if (e.target.id === 'images') {
         let filesList = [];
-        const data = e.target.files;
+        const files = e.target.files;
 
-        for (const key in data) {
-          if (data.hasOwnProperty(key)) {
-            const element = data[key];
-            filesList.push(element);
+        for (const key in files) {
+          if (files.hasOwnProperty(key)) {
+            const element = files[key];
+            filesList.push({image: element});
           }
         }
         // console.log(filesList);
         formData.append('images', filesList);
+        data['images'] = filesList;
       }
       break;
   
     case 'textarea':
       // console.log(e.target.value);
       formData.append('content', e.target.value);
+      data['content'] = e.target.value;
       break;
     
     default:
